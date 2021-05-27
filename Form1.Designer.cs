a85cb6718565049bcb721ca17737f71e5a6f1043
hk-rigacoding <hk.rigacoding@gmail.com>
2021-05-27 21:57:22 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace zive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.level = new System.Windows.Forms.GroupBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.level.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(263, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 222);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(282, 224);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(426, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.Location = new System.Drawing.Point(705, 224);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(28, 222);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // level
+            // 
+            this.level.Controls.Add(this.pictureBox4);
+            this.level.Controls.Add(this.pictureBox2);
+            this.level.Controls.Add(this.pictureBox3);
+            this.level.Controls.Add(this.pictureBox1);
+            this.level.Location = new System.Drawing.Point(12, 12);
+            this.level.Name = "level";
+            this.level.Size = new System.Drawing.Size(1046, 543);
+            this.level.TabIndex = 3;
+            this.level.TabStop = false;
+            this.level.Text = "LEVEL : 1";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox4.Location = new System.Drawing.Point(509, 396);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(422, 50);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(923, 562);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "NEXT";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 586);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.level);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Zive 2021 Edišen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.level.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox level;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zive
+{
+
+    //1. mums ir pašu vadīts personāžs  -   ievade ar taustiņiem <- PA KREISI; PA LABI ->; UZ AUGSHU; UZ LEJU
+    //2. datora vadīti personāži    -   jāuzdod Randoms virziens un pie katras izvēles nogriezties Random();
+    //3. sienas ? loopojams masīvs/kolekcija, jo personāža vadīšanā jānosaka vai nesaskarās !
+
+
+    public partial class Form1 : Form
+    {
+        Pacman pac;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*
+            if (level == null)
+                return;
+
+            foreach (PictureBox pb in level.Controls)
+            {
+                MessageBox.Show(pb.Name);
+            }
+            */
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pac = new Pacman(new Point(100, 100), 10, 0, new Size(50,50));
+            this.Controls.Add(pac);
+            pac.BringToFront();
+        }
+
+        //pac.Left - pac.solis, pac.Right - pac.solis,  pac.Bottom, pac.Top
+        bool Overlap ()
+        {
+            
+            if (level == null)
+             return false;
+
+            /*
+            foreach (PictureBox pb in level.Controls)
+            {
+                if (left >= pb.Left && left <= pb.Right)//kreisā mala ir iekšā svešā kontrolā
+                    if (right >= pb.Left && left <= pb.Right)//kreisā mala ir iekšā svešā kontrolā
+                        return true;
+            }
+
+            */
+
+            foreach (PictureBox pb in level.Controls)
+            {
+                if (pac.Bounds.IntersectsWith(pb.Bounds))
+                    return true;
+            }
+
+
+
+                return false;
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            switch (e.KeyChar)
+            {
+                case 'a'://PA KREISI
+                    pac.virziens = 0;
+                    //ja ir iespēja, mainīt lokāciju 
+                    if (!Overlap())
+                    {
+                        pac.last_loc = pac.Location;
+                        pac.Location = new Point(pac.Location.X - pac.solis, pac.Location.Y);
+                        
+                    } else pac.Location = pac.last_loc;
+
+                    break;
+
+                case 'd'://PA LABI 
+                    pac.virziens = 1;
+
+                    if (!Overlap())
+                    {
+                        pac.last_loc = pac.Location;
+                        pac.Location = new Point(pac.Location.X + pac.solis, pac.Location.Y);
+                    } else pac.Location = pac.last_loc;
+
+                    break;
+
+                case 'w'://UZ AUGŠU
+                    pac.virziens = 2;
+                    if (!Overlap())
+                    {
+                        pac.last_loc = pac.Location;
+                        pac.Location = new Point(pac.Location.X, pac.Location.Y - pac.solis);
+                    } else pac.Location = pac.last_loc;
+
+
+                    break;
+
+                case 's':
+                    pac.virziens = 3;
+
+                    if (!Overlap())
+                    {
+                        pac.last_loc = pac.Location;
+                        pac.Location = new Point(pac.Location.X, pac.Location.Y + pac.solis);
+                    }
+                    else pac.Location = pac.last_loc;
+
+
+                    break;
+
+            }
+
+            e.Handled = true;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+             switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    pac.virziens = 0;
+                    break;
+
+                case Keys.Right:
+                    pac.virziens = 1;
+                    break;
+
+                case Keys.Up:
+                    pac.virziens = 2;
+                    break;
+
+                case Keys.Down:
+                    pac.virziens = 3;
+                    break;
+            }
+        }
+    }
+
+
+    class Pacman : PictureBox
+    {
+
+        //visas bildes
+        Image[,] bildes;
+
+        //virziens (pa kreisi, pa labi, augša, leja)
+        public int virziens;
+
+        //solis ??
+        public int solis;
+
+        //taimeris, kas mainīs spraitus
+        Timer t;
+
+        //spraita indekss 0 - 3
+        int spraits;
+
+        public Point last_loc;
+
+        public Pacman (Point loc, int step, int heading, Size izm)
+        {
+            bildes = new Image[4,4];
+            spraits = 0;
+
+            this.Location = loc;
+            solis = step;
+            virziens = heading;
+
+
+            //pa kreisi
+            bildes[0, 0] = Properties.Resources.pacman_left_1;
+            bildes[0, 1] = Properties.Resources.pacman_left_2;
+            bildes[0, 2] = Properties.Resources.pacman_left_3;
+            bildes[0, 3] = Properties.Resources.pacman_left_4;
+
+            //pa labi
+            bildes[1, 0] = Properties.Resources.pacman_right_1;
+            bildes[1, 1] = Properties.Resources.pacman_right_2;
+            bildes[1, 2] = Properties.Resources.pacman_right_3;
+            bildes[1, 3] = Properties.Resources.pacman_right_4;
+
+            //uz augšu
+            bildes[2, 0] = Properties.Resources.pacman_up_1;
+            bildes[2, 1] = Properties.Resources.pacman_up_2;
+            bildes[2, 2] = Properties.Resources.pacman_up_3;
+            bildes[2, 3] = Properties.Resources.pacman_up_4;
+
+            //uz leju
+            bildes[3, 0] = Properties.Resources.pacman_down_1;
+            bildes[3, 1] = Properties.Resources.pacman_down_2;
+            bildes[3, 2] = Properties.Resources.pacman_down_3;
+            bildes[3, 3] = Properties.Resources.pacman_down_4;
+
+
+            this.Size = izm;
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //spraitu mainīšanai
+            t = new Timer();
+            t.Interval = 150;//50 ms koriģējams !!!
+            t.Tick += T_Tick;
+            t.Start();
+
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            this.Image = bildes[virziens, spraits++];
+
+            if (spraits == 4)
+                spraits = 0;
+
+        }
+    }
+
+
+}
